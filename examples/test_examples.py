24ec07be96fd88d4e41bb3c3a2e93efc20110ed4
Michael Driscoll <mbdriscoll@gmail.com>
2017-06-01 10:22:11 -0700

    added test cases for pics and spmm

---
+++ examples/test_examples.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import pytest
+
+has_spmm_py = os.path.exists("./examples/spmm.py")
+
+@pytest.mark.skipif( not has_spmm_py,
+    reason="requires running pytest from slo root directory")
+def test_spmm():
+    subprocess.run(
+        "python ./examples/spmm.py",
+        shell=True, check=True)
+
+
+has_scan_data = os.path.exists("./scan.h5")
+has_pics_py   = os.path.exists("./examples/pics.py")
+
+@pytest.mark.skipif( not (has_pics_py and has_scan_data),
+    reason="requires running pytest from slo root directory")
+def test_pics():
+    subprocess.run(
+        "python ./examples/pics.py --crop TIME:2,COIL:4 -i 2 --debug 10 scan.h5",
+        shell=True, check=True)
