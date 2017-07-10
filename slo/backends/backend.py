6e20c38b0d05fe846d10441beb9268ec26b3597a
Michael Driscoll <mbdriscoll@gmail.com>
2017-07-10 15:01:22 -0700

    cleanup: apgd reports norm, not residual

--- slo/backends/backend.py
+++ slo/backends/backend.py
@@ -556,5 +556,5 @@ class Backend(object):
                 self.axpy(z, (1-s)/t, o) # z += (1-s)/t*o
 
                 r2 = self.pnorm2(x, team)
-                log.info("iter %d, residual %g", it, r2.real)
+                log.info("iter %d, norm %g", it, abs(r2))
         x.copy_to(x_h)
