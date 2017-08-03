f32ffe8f21b4165963aae50e404d4e4d57939f74
Michael Driscoll <mbdriscoll@gmail.com>
2017-08-03 15:03:26 -0700

    disable building of customgpu module if c library doesn't exist

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
+import os
 from distutils.core import setup, Extension
 import numpy as np
+
+exts = []
 
 customcpu_backend = Extension('slo.backends._customcpu',
     sources = ['slo/backends/_customcpu.c'],
@@ -7,17 +10,20 @@ customcpu_backend = Extension('slo.backends._customcpu',
     extra_compile_args = ['-std=c11', '-fopenmp', '-m64', '-O3'],
     extra_link_args=['-fopenmp', '-mavx'],
 )
+exts.append(customcpu_backend)
 
-customgpu_backend = Extension('slo.backends._customgpu',
-    sources = ['slo/backends/_customgpu.c'],
-    include_dirs=[np.get_include()],
-    extra_compile_args = ['-std=c11'],
-    extra_link_args=['slo/backends/libgpu.a', '-L/usr/local/cuda/lib64', '-lcudart', '-lcublas', '-lstdc++'],
-)
+if os.path.exists( 'slo/backends/libgpu.a' ):
+    customgpu_backend = Extension('slo.backends._customgpu',
+        sources = ['slo/backends/_customgpu.c'],
+        include_dirs=[np.get_include()],
+        extra_compile_args = ['-std=c11'],
+        extra_link_args=['slo/backends/libgpu.a', '-L/usr/local/cuda/lib64', '-lcudart', '-lcublas', '-lstdc++'],
+    )
+    exts.append(customgpu_backend)
 
 setup(
     name = 'slo',
     packages = ['slo', 'slo.backends'],
     version = '1.0.0',
-    ext_modules = [customcpu_backend, customgpu_backend],
+    ext_modules = exts,
 )
