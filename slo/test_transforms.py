91c3df5206869383168b9c9228c3682c930f71fa
Michael Driscoll <mbdriscoll@gmail.com>
2017-08-03 11:29:12 -0700

    bugfix: update name in tests

--- slo/test_transforms.py
+++ slo/test_transforms.py
@@ -52,14 +52,14 @@ def test_Realize_Product(backend, L, M, N, K, density):
 @pytest.mark.parametrize("backend,M,N,K,density",
     list(product( BACKENDS, [3,4], [5,6], [7,8], [1,0.01,0.1,0.5] ))
 )
-def test_StoreMatricesInAdjointOrder(backend, M, N, K, density):
+def test_StoreMatricesInBestOrder(backend, M, N, K, density):
     b = backend()
     A_h = slo.util.randM(M, K, density)
     A = b.SpMatrix(A_h, name='A')
 
-    from slo.transforms import StoreMatricesInAdjointOrder
+    from slo.transforms import StoreMatricesInBestOrder
 
-    AHH = StoreMatricesInAdjointOrder().visit(A)
+    AHH = StoreMatricesInBestOrder().visit(A)
 
     x = b.rand_array((K,N))
     y_act = b.zero_array((M,N), dtype=x.dtype)
