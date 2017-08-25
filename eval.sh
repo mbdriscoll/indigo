1b69b5ebf39359eedb63e70979a42719779897ac
Michael Driscoll <mbdriscoll@gmail.com>
2017-08-25 15:18:14 -0700

    use highres data set for evaluation

--- eval.sh
+++ eval.sh
@@ -1,10 +1,12 @@
 uname -a
 
+export DATA=../data/uwute1_highres.h5
+
 for BACKEND in 'cuda' 'customgpu'; do
   echo $BACKEND
   for OLEVEL in 0 1 2 3 4; do
     echo -n $OLEVEL: ' ' 
-    python examples/pics.py ../data/uwute4_lowres.h5 --crop TIME:1,COIL:4 -i 100 --backend $BACKEND -O$OLEVEL --debug 10 2>&1 | grep ktime | cut -d= -f 2 | tr '\n' ', '
+    python examples/pics.py $DATA --crop TIME:1,COIL:4 -i 100 --backend $BACKEND -O$OLEVEL --debug 10 2>&1 | grep ktime | cut -d= -f 2 | tr '\n' ', '
     echo
   done
 done
