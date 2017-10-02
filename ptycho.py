0a9bff4bc272bed5098a9006acaceb61a4eeb12e
Benjamin Brock <brock@cs.berkeley.edu>
2017-10-02 15:11:39 -0700

    Name the matrices

--- ptycho.py
+++ ptycho.py
@@ -1,6 +1,9 @@
 import h5py
 import numpy as np
+import scipy.sparse as spp
 import ops
+
+import logging
 
 fname = 'CoPd77827_20x20pts_50nmstps_400x10ms_R_RUN3.cxi'
 
@@ -53,32 +56,48 @@ Q,QHQinv = ops.gen_Q(nx, ny, nframes, omega, mapidx)
 psi0 = ops.gen_psi0(Nx, Ny, image)
 z = Q.dot(psi0)
 
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger('ptycho')
+
 # Construct F and Q operators with Indigo.
 from indigo.backends import get_backend
-b = get_backend('numpy')
-F = b.KronI(nframes, b.FFT((nx, ny), dtype=np.complex64))
+b = get_backend('mkl')
+F = b.KronI(nframes, b.FFT((nx, ny), dtype=np.complex64), name='F')
 
-Qop = b.SpMatrix(Q)
-PQ = Qop * b.SpMatrix(QHQinv) * Qop.H
+Qop = b.SpMatrix(Q, name='Q')
+PQ = Qop * b.SpMatrix(QHQinv, name='QHQinv') * b.SpMatrix(Q.H, name='QH')
 
-import matplotlib.pyplot as plt
-plt.ion()
-plt.show()
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+print(Q.shape)
+print(QHQinv.shape)
+
+FHA = F.H * b.SpMatrix(spp.diags(a_data.flatten()), name='A')
+PQFHA = PQ * FHA
+
+# import matplotlib.pyplot as plt
+# fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+
+print('F (2st part): %s' % (F.dump(),))
+print('PQFHA (2nd part): %s' % (PQFHA.dump(),))
+
+import sys
 
 iterations = 10
-import indigo
-p = indigo.util.profile()
+z_d = b.copy_array(z)
+Fz_d = b.zero_array(z.shape, dtype=z.dtype)
+Fz = Fz_d.to_host()
 for i in range(iterations):
-    z_d = b.copy_array(z)
-    Fz_d = b.zero_array(z.shape, dtype=z.dtype)
+    print('Forward eval:')
     F.eval(Fz_d, z_d)
-    Fz = Fz_d.to_host()
-    tmp = a_data.flatten() * (Fz / np.abs(Fz))
-    tmp_d = b.copy_array(tmp)
-    F.eval(z_d, tmp_d, forward=False)
+    print('Done forward eval')
 
-    PQ.eval(tmp_d, z_d, forward=True)
+    Fz_d.copy_to(Fz)
+    Fz = Fz / np.abs(Fz)
+    Fz_d.copy_from(Fz)
+
+    print('Backward eval:')
+    PQFHA.eval(z_d, Fz_d)
+    print('Done backward eval.')
+    '''
     z = tmp_d.to_host()
     psi = Q.H.dot(z)
 
@@ -90,4 +109,4 @@ for i in range(iterations):
     ax2.matshow(np.abs(Fx))
     ax3.matshow(np.abs(psi.reshape(Nx, Ny)))
     ax4.matshow(np.abs(np.fft.fft2(psi.reshape(Nx, Ny))))
-    plt.pause(0.1)
+    '''
